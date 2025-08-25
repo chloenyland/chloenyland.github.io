f7b4b92c91d601f578f66511057ce7ce463cb316
Chloe N <noukonnorth@outlook.com>
2025-08-25 19:18:37 +0000

    Deploy updated site with new _config.yml

--- docs/assets/js/data/swconf.js
+++ docs/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1756148222',resources: [
+    cacheName: 'chirpy-1756149302',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
